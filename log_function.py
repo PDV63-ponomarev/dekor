5b094ed0e7ee74d7781ebf73b6f38de0e7d9fc0b
Dima <graiwers@gmail.com>
2026-02-13 17:00:55 +0400

    continue write

--- log_function.py
+++ log_function.py
@@ -1,16 +1,24 @@
+import types
 
 
 def sentencify(name:str) -> str:
     import re
-    ' '.join(re.split('_+', name))
+    return ' '.join(re.split('_+', name))
 
 
 def step(fn):
     def fn_with_logging(*args, **kwargs) :
         fn_name = sentencify(fn.__name__)
+        is_method = (
+            args
+            and isinstance(args[0], object)
+            and isinstance(getattr(args[0], fn.__name__), types.MethodType)
+        )
+        args_to_log = args[1:] if is_method else args
         print(
-            fn_name
-            + ', '.join(map(str, args)) if args else ''
+            (f'[{args[0].__class__.__name__}] ' if is_method else '')
+            + fn_name
+            + ((': ' + ', '.join(map(str, args_to_log))) if args else '')
         )
         return fn(*args, **kwargs)
 
@@ -31,31 +39,26 @@ given_sign_uo_form_opened = step(given_sign_uo_form_opened)
 
 @step
 def given__sign_uo_form_opened():
-    # log_step(given__sign_uo_form_opened.__name__)
     pass
 
 
 class SignUpForm:
     @step
     def fill_email(self, value):
-        # log_step(self.fill_email.__name__)
         pass
 
     @step
     def fill_password(self, value):
-        # log_step(self.fill_password.__name__)
         pass
 
     @step
     def submit(self):
-        # log_step(self.submit.__name__)
         pass
 
 
 class DashBoard:
     @step
     def go_to_profile(self):
-        # log_step(self.go_to_profile.__name__)
         pass
 
 sign_up_form = SignUpForm()
@@ -63,7 +66,7 @@ dashboard = DashBoard()
 
 
 given__sign_uo_form_opened()
-sign_up_form.fill_email('test@mail.com')
+sign_up_form.fill_email(value='test@mail.com')
 sign_up_form.fill_password('qwerty')
 sign_up_form.submit()
 dashboard.go_to_profile()
